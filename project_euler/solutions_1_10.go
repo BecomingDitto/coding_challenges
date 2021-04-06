ef3807d6608117bbbdb0f9726744ba5d004b3aad
Dan Dubovik <ddubovik@godaddy.com>
2021-04-06 13:17:38 -0700

    Problem 2
    Decided to go with a channel approach.  Mostly just for fun and to practice using them.  It's overkill for this use, and I could have just done the variable swapping with the variables instead of the channels.

--- project_euler/solutions_1_10.go
+++ project_euler/solutions_1_10.go
@@ -1,11 +1,39 @@
 package project_euler
 
+import "errors"
+
 func SolveProblem_1(max int) int {
 	sum := 0
-	for i := 1; i < max; i ++ {
-		if (i % 3 == 0 || i % 5 == 0) {
+	for i := 1; i < max; i++ {
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 	}
 	return sum
+}
+
+func SolveProblem_2(max int) (int, error) {
+	if max > 0 && max < 3 {
+		return 2, nil
+	}
+	num1 := make(chan int, 2)
+	num2 := make(chan int, 2)
+	a, b, c := 1, 2, 0
+	num1 <- a
+	num2 <- b
+	sum := 2
+	for b < max {
+		a = <-num1
+		b = <-num2
+		c = a + b
+		if c >= max {
+			return sum, nil
+		}
+		if c%2 == 0 {
+			sum += c
+		}
+		num1 <- b
+		num2 <- c
+	}
+	return -1, errors.New("Something went really wrong")
 }
